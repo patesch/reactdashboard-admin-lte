2d02abf136dfc1be39e3a3bcee314bb021c09cba
Pedro A M Tesch <patesch@hotmail.com>
2018-01-24 15:58:03 -0200

    Authentication process added

--- backend/src/config/routes.js
+++ backend/src/config/routes.js
@@ -1,16 +1,25 @@
-const express = require('express')
+const express = require("express")
+const auth = require('./auth')
 
 module.exports = function(server) {
+    /*
+    * Rotas protegidas por Token JWT
+    */
+    const protectedApi = express.Router()
+    server.use("/api", protectedApi)
     
-    // Definir a URL base para todas as rotas
-    const router = express.Router()
-    server.use('/api', router)
+    protectedApi.use(auth)
 
-    // Rotas de Ciclo de Pagamento
     const BillingCycle = require('../api/billingCycle/billingCycleService')
-    BillingCycle.register(router, '/billingCycles')
+    BillingCycle.register(protectedApi, '/billingCycles')    
 
-    // Rotas de Creditos
-    const Credit = require('../api/billingCycle/creditService')
-    Credit.register(router, '/credits')
+    /*
+    * Rotas abertas
+    */
+    const openApi = express.Router()
+    server.use('/oapi', openApi)
+    const AuthService = require('../api/user/AuthService')
+    openApi.post('/login', AuthService.login)
+    openApi.post('/signup', AuthService.signup)
+    openApi.post('/validateToken', AuthService.validateToken)    
 }
--- frontend/src/main/routes.jsx
+++ frontend/src/main/routes.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Router, Route, Redirect, IndexRoute, hashHistory } from 'react-router'
 
-import App from './app'
+import AuthOrApp from './authOrApp'
+// import App from './app'
 import Dashboard from '../dashboard2/dashboard2'
 import BillingCycles from '../billingCycle/billingCycle'
 
 const routes = props => (
     <Router history={hashHistory} >
-        <Route path="/" component={App}>
+        <Route path="/" component={AuthOrApp}>}>
             <IndexRoute component={Dashboard} />
             <Route path="billingCycles" component={BillingCycles} />
         </Route>
